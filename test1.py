577aaa36c3ad3a8746cb185865cb4efa3a6b032e
Firoz Pathan <firoz.pathan@netsuite.com>
2022-04-04 12:56:54 +1000

    Added another comment line from doc branch

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 """Comment made from documentation branch"""
 print("Hello world")
+"""Included this from documentation branch"""
 print("Main branch")
 """Added doc from Master branch """
 print("Develop Branch")
